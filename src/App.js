a3e8e9735def511bbba6eab48d10374993520572
Mansur Shoev <mansurzur103@gmail.com>
2022-03-26 15:48:54 +0300

    new update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,59 @@
 import "./App.css";
 import { useState } from "react";
-import Couter from "./components/couter";
 
 function App() {
-    const [todos,setTodos]=useState([])
-    const [text,setText]=useState("")
-    const hedlChange = (e)=>{
-        const {value} = e.target
-        setText(value)
-    }
-    const add = (prev)=>{
-     if(text== ""){
-         return
-     }
-    const nemtodo ={
-      text,
-      isCompleted:false
-    }
-    return [] 
-    
-    }
-  return(
-      <>
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+  const hendlChenge = (e) => {
+    const { value } = e.target;
+    setText(value);
+  };
+  const add = () => {
+    if (text === "") return;
+    setTodos((perv) => [...perv, { text, isCompleted: false }]);
+    setText("");
+  };
+  const complete = (index) => {
+    const copy = [...todos];
+
+    copy[index].isCompleted = copy[index].isCompleted ? false : true;
+    console.log(copy, "copy");
+    setTodos(copy);
+  };
+
+  const remove = () => {
+    const copy = [...todos];
+    copy.splice(index, 1);
+    setTodos(copy);
+  };
+
+  return (
+    <>
       <div className="app">
-          <div className="todo-list">
-          <input value={text} onChange={hedlChange} /> 
-          <button>add</button>
-          </div>
+        <div className="todo-list">
+          <input value={text} onChange={hendlChenge} />
+          <button onClick={add}>add</button>
+          {todos.map((e, index) => {
+            return (
+              <div className="todo" key={index}>
+                <p
+                  style={
+                    e.isCompleted ? { textDecoration: "line-through" } : {}
+                  }
+                >
+                  {e.text}
+                </p>
+                <div>
+                  <button onClick={() => complete(index)}>complete</button>
+                  <button onClick={() => remove(index)}>delete</button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      </>
-  )
+    </>
+  );
 }
 
 export default App;

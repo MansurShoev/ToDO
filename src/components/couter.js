9c4c4f5bbc58f1a730e6047605aec95f7bd2b924
Mansur Shoev <mansurzur103@gmail.com>
2022-03-26 14:26:03 +0300

    ToDO

---
+++ src/components/couter.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+
+
+function Couter (){
+ 
+
+
+    return <>
+    <div>
+        <input  />
+        <button>add</button>
+    </div>
+    </>  
+}
+export default Couter
--- src/components/img.js
+++ src/components/img.js
@@ -1,27 +1,5 @@
 
 
-function Image (props) {
-    return  <div className="w3-third w3-margin-bottom">
-    <img
-      src={surat}
-      alt={props.imia}
-      style={{ width: "100%" }}
-      className="w3-hover-opacity"
-    />
-    <div className="w3-container w3-white">
-      <p>
-        <b>{props.imia}</b>
-      </p>
-      <p className="w3-opacity">Fri 27 Nov 2016</p>
-      <p>
-        Praesent tincidunt sed tellus ut rutrum sed vitae justo.
-      </p>
-      <button className="w3-button w3-black w3-margin-bottom">
-      {props.text}
-      </button>
-    </div>
-  </div>
-}
 
 
 export default Image

523a93c6b75b136461f1833bd10573f04e138af8
Mansur Shoev <mansurzur103@gmail.com>
2022-03-27 15:13:02 +0300

    .gitignore

--- src/App.js
+++ src/App.js
@@ -2,58 +2,16 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [text, setText] = useState("");
-  const hendlChenge = (e) => {
-    const { value } = e.target;
-    setText(value);
-  };
-  const add = () => {
-    if (text === "") return;
-    setTodos((perv) => [...perv, { text, isCompleted: false }]);
-    setText("");
-  };
-  const complete = (index) => {
-    const copy = [...todos];
+ 
 
-    copy[index].isCompleted = copy[index].isCompleted ? false : true;
-    console.log(copy, "copy");
-    setTodos(copy);
-  };
-
-  const remove = (index) => {
-    const copy = [...todos];
-    copy.splice(index, 1);
-    setTodos(copy);
-  };
-
-  return (
-    <>
-      <div className="app">
-        <div className="todo-list">
-          <input value={text} onChange={hendlChenge} />
-          <button onClick={add}>add</button>
-          {todos.map((e, index) => {
-            return (
-              <div className="todo" key={index}>
-                <p
-                  style={
-                    e.isCompleted ? { textDecoration: "line-through" } : {}
-                  }
-                >
-                  {e.text}
-                </p>
-                <div>
-                  <button onClick={() => complete(index)}>complete</button>
-                  <button onClick={() => remove(index)}>delete</button>
-                </div>
-              </div>
-            );
-          })}
-        </div>
+  return <>
+    <div className="app">
+      <div className="todo-lit">
+        <input  />
+        <button                                                                                                                                       ggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg>add</button>
       </div>
-    </>
-  );
+    </div>
+  </>
 }
 
 export default App;
